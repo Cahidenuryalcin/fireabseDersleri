9fd924139ffcc9501ab2874b8482e385ac345f44
Cahidenuryalcin <cnuryal@gmail.com>
2024-05-30 00:46:12 +0300

    29.05.24

--- lib/firestore_dersleri.dart
+++ lib/firestore_dersleri.dart
@@ -4,10 +4,15 @@ import 'package:flutter/material.dart';
 class FirestoreDersleri extends StatelessWidget {
   FirestoreDersleri({super.key});
 
-  FirebaseFirestore _firestore= FirebaseFirestore.instance;
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
+
+    // IDler
+    debugPrint(_firestore.collection("users").id);
+    debugPrint(_firestore.collection("users").doc().id); // add ile bir şey değiştirildiğinde farklı farklı idler oalrak gelerek değişiyor, her çalıştığında
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Firestore işlemleri"),
@@ -16,9 +21,12 @@ class FirestoreDersleri extends StatelessWidget {
         child: Column(
           children: [
             ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
                 onPressed: () => veriEklemeAdd(),
                 child: Text("Veri Ekleme Add")),
-                ElevatedButton(
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red.shade300),
                 onPressed: () => veriEklemeSet(),
                 child: Text("Veri Ekleme Set"))
           ],
@@ -28,14 +36,34 @@ class FirestoreDersleri extends StatelessWidget {
   }
 
   veriEklemeAdd() async {
-   Map<String, dynamic> _eklenecekUser= Map<String, dynamic>();
-   _eklenecekUser["isim"]= "cahide";
-   _eklenecekUser["yas"]= 23;
-   _eklenecekUser["ogrenciMi"]= false;
-   await  _firestore.collection("users").add({
+    Map<String, dynamic> _eklenecekUser = Map<String, dynamic>();
+    _eklenecekUser["isim"] = "menes";
+    _eklenecekUser["yas"] = 23;
+    _eklenecekUser["ogrenciMi"] = true;
+    _eklenecekUser["adres"] = {"il": "bursa", "ilce": "kestel"};
+    _eklenecekUser["renkler"] = FieldValue.arrayUnion(["turkuaz", "yesil"]);
+    _eklenecekUser["createdAt"] = FieldValue.serverTimestamp();
 
-   })
+    await _firestore.collection("users").add(_eklenecekUser);
   }
-  
-  veriEklemeSet() async{}
+
+  veriEklemeSet() async {
+
+
+    // veritabanında da ID değerini tutabilmek için
+    var _yediDocId= _firestore.collection("users").doc().id;
+    await _firestore.doc("users/$_yediDocId").set({
+      "isim": "menes",
+      "userID":_yediDocId
+
+    });
+
+
+
+
+     Map<String, dynamic> _kullaniciyaBaskaOzellikEkleme = Map<String, dynamic>();
+     _kullaniciyaBaskaOzellikEkleme["okul"]= "Bursa Uludag Universitesi";
+     _kullaniciyaBaskaOzellikEkleme["yas"]= FieldValue.increment(1); // counter gibi çalışıyor. yas degerini alıcak +1 yaparak yerine koyacak
+    await _firestore.doc("users/ixTTLxcObuTx4aPagpH0").set(_kullaniciyaBaskaOzellikEkleme,  SetOptions(merge: true));
+  }
 }
